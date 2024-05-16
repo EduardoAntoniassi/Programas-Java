8575ff4baf997978adc145480d34f827c301807d
Eduardo Antoniassi <eduardoantoniassi0@gmail.com>
2024-05-15 22:03:01 -0300

    First Commit
    Exercício 1 dia 15/05

---
+++ Programação Orientada a Objetos/ex1dia15.java
@@ -0,0 +1,43 @@
+public class ex1dia15 {
+
+    //Aluno: Eduardo Lourenço Antoniassi;
+    
+    private boolean status;
+
+    public ex1dia15() {
+        this.status = false;
+
+    }
+
+    public void ligar(int tempo) {
+  
+        this.status = true;
+        
+        for (int i = tempo; i > 0; i--) {
+            System.out.println("Bomba ligada. Tempo Restante: " + i + " segundos.");
+            
+            try {
+                Thread.sleep(1000);
+            
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+        }
+        
+        desligar();
+
+    }
+
+    public void desligar() {
+        this.status = false;
+        System.out.println("Bomba desligada.");
+
+    }
+
+    public static void main(String[] args) {
+        ex1dia15 bomba = new ex1dia15();
+        bomba.ligar(10); 
+
+    }
+}
